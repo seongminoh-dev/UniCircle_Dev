7b95d603662220a69a7fef7171724ef9f298bea7
Jaewon <20011211s@gmail.com>
2024-09-24 10:42:08 +0900

    Feat: backend 폴더 생성

---
+++ backend/src/test/java/uniCircle/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uniCircle.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
