cd6ba08621bfffe252148cadc15b1a6c593dd52a
baeyc0510 <bjj5573@gmail.com>
2024-11-25 20:23:06 +0900

    Refactor: 중복 layout삭제
    로그인 제외 페이지 (user) 폴더로 이동

--- frontend/src/app/(user)/template.jsx
+++ frontend/src/app/(user)/template.jsx
@@ -3,6 +3,7 @@
 import { AuthGuard } from "@/guards/authGuard";
 import { useAuth } from "@/hooks";
 import { Loading } from "@/components";
+import {Header, LeftSidebar, RightSidebar, SearchBar} from "@/components";
 
 const SplashScreen = () => {
     return (
@@ -33,9 +34,22 @@ export default function Template({ children }) {
           <SplashScreen />
         ) : (
           <>
-            <AuthGuard>
-              <>{children}</>
-            </AuthGuard>
+            {/*<AuthGuard>*/}
+            <div>
+              <Header/>
+              <div className="z-10 min-h-screen flex space-x-4">
+                <LeftSidebar />
+                <div className="h-screen flex flex-col flex-1 p-4 ">
+                  <SearchBar/>
+                  {/* 내용물(children) 목록 스크롤 가능 영역 */}
+                  <div className="overflow-y-auto p-4 space-y-4 flex-grow">
+                    {children}
+                  </div>
+                </div>
+                <RightSidebar />
+              </div>
+            </div>
+            {/*</AuthGuard>*/}
           </>
         )}
       </>
