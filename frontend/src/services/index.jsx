e588c85fbebc2ce366b49255566f2fa2394a877c
SeongminOh <seongminoh.dev@gmail.com>
2024-11-30 03:27:19 +0900

    Refactor: Service Index 정리

--- frontend/src/services/index.jsx
+++ frontend/src/services/index.jsx
@@ -1,14 +1,9 @@
-import { Login, checkUserAuthentication } from './Auth';
 import { setCookie, getCookie, getAllCookies, deleteCookies } from './Cookie';
-import { Logout } from './Logout';
 import { PostData } from './PostData';
 export {
   deleteCookies,
-  Login,
-  Logout,
   setCookie,
   getAllCookies,
   getCookie,
-  checkUserAuthentication,
   PostData,
 };
