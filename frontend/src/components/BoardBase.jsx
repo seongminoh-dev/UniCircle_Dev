56ec7bf69bf7c515d92146799c97349179bdae12
baeyc0510 <baeyc0510@gmail.com>
2024-12-06 17:09:10 +0900

    Design: Board 레이아웃 수정

--- frontend/src/components/BoardBase.jsx
+++ frontend/src/components/BoardBase.jsx
@@ -40,33 +40,6 @@ const BoardBase = ({ board, isPreview = false }) => {
       {/* Title & Content */}
       <h2 className="text-xl font-bold mb-2">{title}</h2>
       <p className={`text-gray-700 mb-4 mx-1 ${isPreview ? "line-clamp-3" : ""}`}>{displayContent}</p>
-
-      {/* Tags and Stats */}
-      <div className="flex justify-between items-center">
-      
-
-        {/* Views and Comments */}
-        <div className="flex items-center space-x-4 text-gray-500 mx-4">
-              {/* Comments */}
-          <div className="flex items-center space-x-1">
-            <svg
-              width="15"
-              height="15"
-              viewBox="0 0 15 15"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M13.125 9.375C13.125 9.70652 12.9933 10.0245 12.7589 10.2589C12.5245 10.4933 12.2065 10.625 11.875 10.625H4.375L1.875 13.125V3.125C1.875 2.79348 2.0067 2.47554 2.24112 2.24112C2.47554 2.0067 2.79348 1.875 3.125 1.875H11.875C12.2065 1.875 12.5245 2.0067 12.7589 2.24112C12.9933 2.47554 13.125 2.79348 13.125 3.125V9.375Z"
-                stroke="#808080"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <span className="text-sm">{comments}</span>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
