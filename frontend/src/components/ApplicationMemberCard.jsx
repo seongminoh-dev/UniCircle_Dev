d4bb3ed6ff4a0f5e1dbb8df1ad9185bcd57ddb0a
baeyc0510 <baeyc0510@gmail.com>
2024-12-06 15:39:58 +0900

    Feat: 내 동아리 관리 페이지 완성

--- frontend/src/components/ApplicationMemberCard.jsx
+++ frontend/src/components/ApplicationMemberCard.jsx
@@ -1,11 +1,12 @@
 "use client";
 
 import MemberCard from "./MemberCard";
+import dayjs from "dayjs";
 
 // 가입신청 멤버 카드
 const ApplicationMemberCard = ({ nickname, date, onViewForm, onApprove, onReject }) => {
   return (
-    <MemberCard nickname={nickname} date={`${date} 가입 신청`}>
+    <MemberCard nickname={nickname} date={`${dayjs(date).format("YYYY년 MM월 DD일")} 가입 신청`}>
       <button
         onClick={onViewForm}
         className="bg-blue-500 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-blue-600"
--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -1,24 +1,33 @@
 'use client';
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useAuth } from '@/hooks';
+import { useRouter } from 'next/navigation'; // Next.js의 useRouter 사용
 
 export const LeftSidebar = () => {
-  // 항목 리스트를 배열로 저장
-  const items = ['나와 관련된 게시물','관심 동아리', '동아리 관리', '설정'];
+  const items = ['나와 관련된 게시물', '관심 동아리', '동아리 관리', '설정'];
   const { user } = useAuth();
   const nickname = user?.nickname || '익명';
   const [selectedItem, setSelectedItem] = useState(items[0]);
+
+  const router = useRouter(); // useRouter 훅 초기화
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    // '동아리 관리' 버튼이 눌리면 특정 경로로 이동
+    if (item === '동아리 관리') {
+      router.push('/circle-management/'); // 원하는 경로로 라우팅
+    }
+  };
 
   return (
     <aside className="w-80 h-screen py-8 pl-6">
       {/* 닉네임 및 활동 정보 */}
       <div className="mb-2 space-y-4">
         <h3 className="text-3xl font-semibold">{nickname}</h3>
-        <hr className="border-t-2 border-gray-300 my-2" />   {/* 가로선 추가 */}
+        <hr className="border-t-2 border-gray-300 my-2" /> {/* 가로선 추가 */}
         <p className="text-base text-gray-600 font-semibold ">내 활동</p>
       </div>
-      
+
       {/* 가입 정보 */}
       <div className="mb-6">
         <p className="text-sm text-gray-500">
@@ -28,13 +37,10 @@ export const LeftSidebar = () => {
 
       {/* 관련된 게시물 목록 (탐색 메뉴로 사용) */}
       <nav className="bg-white space-y-2">
-        {/* map으로 반복하여 각각의 버튼 생성 */}
         {items.map((item, index) => (
           <button
             key={index}
-            // 클릭 시 선택된 아이템 상태 업데이트
-            onClick={() => setSelectedItem(item)}
-            // 선택된 상태일 때와 아닐 때의 스타일 구분
+            onClick={() => handleItemClick(item)} // 클릭 이벤트 핸들러
             className={`block w-full text-center py-2 px-4 rounded-lg shadow cursor-pointer
               ${selectedItem === item ? 'bg-blue-500 text-white' : 'bg-white text-gray-700 hover:bg-gray-50'}`}
           >
@@ -45,4 +51,5 @@ export const LeftSidebar = () => {
     </aside>
   );
 };
+
 export default LeftSidebar;
