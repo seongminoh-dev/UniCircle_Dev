39c123f4e6f480fef39583ebebeccfa59b827389
SeongminOh <seongminoh.dev@gmail.com>
2024-12-08 03:27:21 +0900

    Feat: 나와 관련된 게시물 업데이트

--- frontend/src/app/(user)/boards/related/page.jsx
+++ frontend/src/app/(user)/boards/related/page.jsx
@@ -3,11 +3,14 @@
 import React, { useEffect, useState } from "react";
 import BoardPreview from "@/components/BoardPreview";
 import { getAllPosts } from "@/services/Board"; // getAllPosts 함수 불러오기
+import { getEncounteredCircle } from "@/services";
+import { useAuth } from "@/hooks";
 
 const Board = () => {
   const [boards, setBoards] = useState([]); // 게시글 상태
   const [loading, setLoading] = useState(true); // 로딩 상태
   const [error, setError] = useState(null); // 에러 상태
+  const auth = useAuth();
 
   // 게시글 데이터 가져오기
   useEffect(() => {
@@ -15,7 +18,14 @@ const Board = () => {
       setLoading(true);
       try {
         const data = await getAllPosts(); // 서버에서 게시글 데이터 가져오기
-        setBoards(data); // 상태에 데이터 설정
+        const member = await getEncounteredCircle(auth.user.email);
+        const circle = member.map(item => item.circleId);
+        const filteredData = data.filter((item) => {
+        const { visibility, circleId } = item.board;
+          //return visibility !== "PRIVATE" || circle.includes(circleId);
+          return circle.includes(circleId);
+        });
+        setBoards(filteredData); // 상태에 데이터 설정
       } catch (err) {
         setError("게시글을 불러오는 중 오류가 발생했습니다.");
         console.error(err);
@@ -29,8 +39,9 @@ const Board = () => {
   return (
     <div className="space-y-4">
       <h2 className="text-xl font-semibold text-gray-500 p-2 mb-2">나와 관련된 게시물</h2>
-      {loading && <p>로딩 중...</p>}
+      {loading && <p>게시글을 불러오는 중...</p>}
       {error && <p>{error}</p>}
+      {!loading && boards.length===0 && <p>나와 관련된 게시물이 없습니다. 동아리에 가입하고 다양한 게시물을 열람하세요!</p>}
       {!loading &&
         boards.map((board, index) => (
           <BoardPreview key={index} board={board} />
