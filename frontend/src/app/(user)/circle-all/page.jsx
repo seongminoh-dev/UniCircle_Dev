784b8344a57982b3ce9e944d8019458e35d8ce9f
baeyc0510 <bjj5573@gmail.com>
2024-12-08 17:10:56 +0900

    Feat: 동아리 탐색 페이지 제작

---
+++ frontend/src/app/(user)/circle-all/page.jsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/hooks";
+import CirclePreview from "@/components/CirclePreview";
+import { getCircles } from "@/services";
+
+const CircleListPage = () => {
+  const router = useRouter();
+  const auth = useAuth();
+  
+  const [circles, setCircles] = useState([]);
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      alert("로그인이 필요합니다.");
+      router.push("/auth/login");
+      return;
+    }
+
+    const fetchData = async () => {
+      try {
+        const allCircles = await getCircles();
+        const structuredCircles = allCircles.map(circle => ({
+            id: circle.circleId,
+            name: circle.name,
+            image: circle.imageUrl || "https://via.placeholder.com/150",
+            tags: circle.hashtags || [],
+            description: circle.description,
+          }));
+        setCircles(structuredCircles);
+      } catch (error) {
+        console.error(error);
+        alert("데이터 로딩에 실패했습니다.");
+      }
+    };
+
+    fetchData();
+  }, [auth,router]);
+
+  return (
+    <div className="p-2">
+      <h1 className="text-2xl font-bold text-gray-900 mb-2">동아리 탐색</h1>
+
+      <div className="space-y-2">
+          {!circles || circles.length === 0 ? (
+            <div className="bg-gray-100 p-4 space-y-4 rounded-lg">
+            <p className="text-gray-500">아직 동아리가 없습니다.</p>
+            </div>
+          ) : (
+            circles.map((circle, index) => (
+              <CirclePreview key={index} circle={circle} />
+            ))
+          )}
+        </div>
+    </div>
+  );
+};
+
+export default CircleListPage;
